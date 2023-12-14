64bc16b25c308343fd99fdeb6ecc17cd943515ad
1blckhrt <1blckhrt1@gmail.com>
2023-12-14 18:01:18 -0500

    12/14

--- Lesson 7/dictionaries.py
+++ Lesson 7/dictionaries.py
@@ -75,3 +75,78 @@ print(band2)
 band3 = dict(band)
 print("Good copy as well!")
 print(band3)
+
+# nested dictionaries
+member1 = {
+    "name": "Plant",
+    "instrument": "vocals"
+}
+
+member2 = {
+    "name": "Page",
+    "instrument": "guitar"
+}
+
+band = {
+    "member1": member1,
+    "member2": member2
+}
+
+print(band)
+print(band['member1']['name'])
+
+# Sets
+
+nums = { 1, 2, 3, 4}
+
+nums2 = set((1, 2, 3, 4))
+print(nums)
+print(nums2)
+print(type(nums))
+print(len(nums))
+
+# No duplicates are allowed
+nums = { 1, 2, 2, 3 }
+print(nums) # ignores duplicates
+
+# True is a duplicate of 1, False is a duplicate of zero
+
+nums = { 1, True, 2, False, 3, 4, 0 }
+print(nums)
+
+# check if a value is in a set
+print(2 in nums)
+
+# but you cannot refer to an element in the set with an index position or a key
+
+# Add a new element to a set
+nums.add(8)
+print(nums)
+
+# Add elements from one set to another
+morenums = { 5, 6, 7 }
+nums.update(morenums)
+print(nums)
+
+# you can use update with lists, tuples, and dictionaries too
+
+# merge two sets to create a new set
+one = { 1, 2, 3 }
+two = { 5, 6, 7 }
+
+mynewset = one.union(two)
+print(mynewset)
+
+# keep only the duplicates
+one = { 1, 2, 3 }
+two = { 2, 3, 4 }
+
+one.intersection_update(two)
+print(one)
+
+# keep everything except duplicates
+one = { 1, 2, 3 }
+two = { 2, 3, 4 }
+
+one.symmetric_difference_update(two)
+print(one) 
---
+++ Lesson 8/loops.py
@@ -0,0 +1,21 @@
+value = 1
+# while value <= 10:
+#     print(value)
+#     if value ==5:
+#         break
+#     value += 1
+
+# while value <= 10:
+#     value += 1
+#     if value ==5:
+#         continue
+#     print(value)
+# else:
+#     print('Value is now equal to ' + str(value))
+
+names = ["Dave", "Sara", "John"]
+for x in names:
+    print(x)
+
+for x in "Mississippi":
+    print(x)
