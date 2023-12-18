f35c649128d5977e255d48aba99fe4d8b64d8f96
1blckhrt <1blckhrt1@gmail.com>
2023-12-17 19:50:12 -0500

    12/17

--- Lesson 11/rps4.py
+++ Lesson 11/rps4.py
@@ -2,6 +2,8 @@ import sys
 import random
 from enum import Enum
 
+game_count = 0
+
 def play_rps():
 
     class RPS(Enum):
@@ -25,16 +27,26 @@ def play_rps():
     print("\nYou chose " + str(RPS(player)).replace('RPS.', '').title() + ".")
     print("Python chose " + str(RPS(computer)).replace('RPS.', '').title() + ".\n")
 
-    if player == 1 and computer == 3:
-        print("🎉 You win!")
-    elif player == 2 and computer == 1:
-        print("🎉 You win!")
-    elif player == 3 and computer == 2:
-        print("🎉 You win!")
-    elif player == computer:
-        print("😲 Tie game!")
-    else:
-        print("🐍 Python wins!")
+    def decide_winner(player, computer):
+        if player == 1 and computer == 3:
+            return "🎉 You win!"
+        elif player == 2 and computer == 1:
+            return "🎉 You win!"
+        elif player == 3 and computer == 2:
+            return "🎉 You win!"
+        elif player == computer:
+            return "😲 Tie game!"
+        else:
+            return "🐍 Python wins!"
+
+    game_result = decide_winner(player, computer)
+
+    print(game_result)
+
+    global game_count
+    game_count += 1
+
+    print("\nGame count: " + str(game_count))
 
     print("\nPlay again?")
 
---
+++ Lesson 11/scope.py
@@ -0,0 +1,18 @@
+name = "Dave"
+count = 1
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("Dave")
+
+another()
